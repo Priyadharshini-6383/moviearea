f0985f77fa6bba802de0b4968132a93118d552d9
Priyadharshini-6383 <pm.priyadharshini123@gmail.com>
2025-09-02 22:35:34 +0530

    2nd change

--- main.js
+++ main.js
@@ -8,6 +8,26 @@ app.get('/' , (req , res) => {
 res.json({message : "Hello students!"});
 });
 
+// read
+app.get('/movies' , (req ,res) => {
+
+});
+
+// create
+app.post('/movies', (req , res) => {
+
+});
+
+// update
+app.put('/movies/:id' , (req , res) => {
+
+});
+
+// delete
+app.delete('/movies/:id' , (req , res) => {
+
+});
+
 app.listen(PORT , () => {
     console.log(`the message http://localhost : ${PORT}`);
 })
