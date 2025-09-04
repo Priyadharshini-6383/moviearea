3301876d53d4ced7cd8f6444332b4ca2f1029267
Priyadharshini-6383 <pm.priyadharshini123@gmail.com>
2025-09-04 20:15:11 +0530

    moved controller and library

--- routes/movies.route.js
+++ routes/movies.route.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 
-const MovieRead = require("./controllers/movies.controllers.js");
-const MovieCreate = require("./controllers/movies.controllers.js");
-const MovieUpdate = require("./controllers/movies.controllers.js");
-const MovieDelete = require("./controllers/movies.controllers.js");
+const MovieRead = require("../controllers/movies.controllers.js");
+const MovieCreate = require("../controllers/movies.controllers.js");
+const MovieUpdate = require("../controllers/movies.controllers.js");
+const MovieDelete = require("../controllers/movies.controllers.js");
 
 
 //read
